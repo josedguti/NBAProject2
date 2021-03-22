ebe78c094d2959736b89c96cae2969bb552198e0
Jose Gutierrez <josegutierrez@Joses-Mac-mini.fios-router.home>
2021-03-22 13:52:33 -0400

    fix: adds env var access for port

--- routes/teams.js
+++ routes/teams.js
@@ -7,5 +7,8 @@ router.get('/', teamsCtrl.index);
 router.get('/new', teamsCtrl.new);
 router.get('/:id', teamsCtrl.show);
 router.post('/', teamsCtrl.create);
+router.delete('/:id', teamsCtrl.delete);
+router.get('/:id/edit', teamsCtrl.edit);
+router.put('/:id', teamsCtrl.update);
 
 module.exports = router;
